be48c3edffc120e40d74cd0e0a12f98fb4d12ae5
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-05-29 19:31:52 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+//! Nanonaut example in rust
+
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+fn main() -> amethyst::Result<()> {
     println!("Hello, world!");
+    Ok(())
 }
